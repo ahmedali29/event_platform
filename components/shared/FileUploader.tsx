8dc694485b4e39e29c2ed58262ab93f296026d6d
Ahmed Ali <36512007+ahmedali29@users.noreply.github.com>
2024-02-06 18:31:28 +0200

    Update FileUploader.tsx

--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import { useCallback, Dispatch, SetStateAction } from 'react'
-import type { FileWithPath } from '@uploadthing/react'
+
+/* import type { FileWithPath } from '@uploadthing/react' */
+
 import { useDropzone } from '@uploadthing/react/hooks'
 import { generateClientDropzoneAccept } from 'uploadthing/client'
 
