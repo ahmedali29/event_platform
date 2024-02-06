465587ff45f9a2f4c7ba8ceb3efcc0b460ce9b47
Ahmed Ali <36512007+ahmedali29@users.noreply.github.com>
2024-02-06 18:39:15 +0200

    Update FileUploader.tsx

--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -17,7 +17,7 @@ type FileUploaderProps = {
 }
 
 export function FileUploader({ imageUrl, onFieldChange, setFiles }: FileUploaderProps) {
-  const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
     setFiles(acceptedFiles)
     onFieldChange(convertFileToUrl(acceptedFiles[0]))
   }, [])
